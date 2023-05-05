37aad940aaf5004e1c7ebc070f298d8fc87ec894
Jeral Pineda <jeral.cris@gmail.com>
2023-05-04 18:31:55 -0600

    fix: Agregando useRef

--- src/bases/CounterEffect.tsx
+++ src/bases/CounterEffect.tsx
@@ -1,13 +1,16 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 const MAXIMUN_COUNT = 10;
 
 export const CounterEffect = () => {
   const [counter, setCounterState] = useState(5);
+  const counterRef = useRef<HTMLHeadingElement>(null);
 
   const handleClick = () => {
     setCounterState((prev) => Math.min(prev + 1, MAXIMUN_COUNT));
   };
+
+  console.log(counterRef.current);
 
   useEffect(() => {
     if (counter <= 10) return;
@@ -18,7 +21,7 @@ export const CounterEffect = () => {
   return (
     <>
       <h1>CounterEffect:</h1>
-      <h2>{counter}</h2>
+      <h2 ref={counterRef}>{counter}</h2>
 
       <button onClick={handleClick}>+1</button>
     </>
