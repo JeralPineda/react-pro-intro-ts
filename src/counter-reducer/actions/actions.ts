81112139e177bef5404f89c24bc6c632973a9bc7
Jeral Pineda <jeral.cris@gmail.com>
2023-05-04 22:24:54 -0600

    feat: Agregando Actions Creators

--- src/counter-reducer/actions/actions.ts
+++ src/counter-reducer/actions/actions.ts
@@ -1,2 +1,16 @@
 //Type porque no va crecer, no va cambiar y no va a extenderse para otras cosas
 export type CounterAction = { type: 'increaseBy'; payload: { value: number } } | { type: 'reset' };
+
+// export const doReset = (): CounterAction => {
+//   return {
+//    type: 'reset'
+//   };
+// };
+export const doReset = (): CounterAction => ({
+  type: 'reset',
+});
+
+export const doIncreaseBy = (value: number): CounterAction => ({
+  type: 'increaseBy',
+  payload: { value },
+});
